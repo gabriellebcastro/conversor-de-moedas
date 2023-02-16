ed5fbfd818421eb218624287d1d3d14700580493
Gabrielle Castro <gabriellebcastro@gmail.com>
2023-02-16 13:24:39 -0300

    teste - conversão

--- src/app/conversor/conversor.component.spec.ts
+++ src/app/conversor/conversor.component.spec.ts
@@ -10,6 +10,8 @@ import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSelectModule } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
+import { CurrencyListService } from './../services/currency-list.service';
+import { of } from 'rxjs';
 
 describe('ConversorComponent', () => {
   let component: ConversorComponent;
@@ -30,11 +32,30 @@ describe('ConversorComponent', () => {
         MatSelectModule,
         FormsModule
       ],
+      providers: [CurrencyListService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ConversorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  let currencyList: CurrencyListService;
+
+  beforeEach(() => {
+    currencyList = TestBed.inject(CurrencyListService);
+    spyOn(currencyList, 'convert').and.returnValue(of({ result: 100, info: { rate: 2 } }));
+    component = new ConversorComponent(currencyList);
+    component.moedaOrigem = 'USD';
+    component.moedaDestino = 'BRL';
+    component.valor = 50;
+  });
+
+  it('should convert currency', () => {
+    component.convert();
+    expect(component.data).toEqual(jasmine.any(Date));
+    expect(component.resultado).toEqual(100);
+    expect(component.taxa).toEqual([2]);
   });
 
   it('should create', () => {
